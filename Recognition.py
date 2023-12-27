a10b7ff39324764a6d1b56a0542b3cc52f9036a9
Paulius Bakšys <75304721+MrPaska@users.noreply.github.com>
2023-12-27 19:59:15 +0200

    Update Recognition.py

--- Recognition.py
+++ Recognition.py
@@ -1,59 +1,56 @@
 import cv2
 import tensorflow as tf
 import numpy as np
+from tensorflow.lite.python.interpreter import Interpreter
 
-tflite_model_file = "best-fp16_2.tflite"
-with open(tflite_model_file, "rb") as fid:
-    tflite_model = fid.read()
+tflite_model = "best-fp16_2.tflite"
+label_map_file = "labelmap.txt"
+
+with open(label_map_file, "r") as f:
+    labels = [line.strip() for line in f.readlines()]
 
 # Load TFLite model
-module = tf.lite.Interpreter(model_content=tflite_model)
+module = Interpreter(model_path=tflite_model)
 module.allocate_tensors()
 
 # Getting input and output tensors
-input_dtl = module.get_input_details()
-output_dtl = module.get_output_details()
-
-classes = ["momentinis", "vidutinis", "zona"]
+input_details = module.get_input_details()
+output_details = module.get_output_details()
 
 cap = cv2.VideoCapture(0)
-#cap.set(cv2.CAP_PROP_FRAME_WIDTH, 720)
-#cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
+# cap.set(cv2.CAP_PROP_FRAME_WIDTH, 720)
+# cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
 
 while True:
-    success, img = cap.read()
-    if success:
-        cv2.imshow("frame", img)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-    else:
-        print("Camera failed!")
+    success, frame = cap.read()
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
     # Preprocessing the frame
-    input_data = cv2.resize(img, (640, 640), interpolation=cv2.INTER_CUBIC)
-    #input_data = input_data.astype(np.float32)  # Convert to float32
-    input_data = input_data.astype(np.float32)
-    input_data = np.expand_dims(input_data, axis=0)
+    frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    frame_resized = cv2.resize(frame, (640, 640), interpolation=cv2.INTER_CUBIC)
+    frame_type = frame_resized.astype(np.float32)
+    input_data = np.expand_dims(frame_type, axis=0)
 
     # Transposing dimensions
-    #input_data = np.transpose(input_data, (0, 3, 1, 2))
+    # input_data = np.transpose(input_data, (0, 3, 1, 2))
 
-    print(img.shape)
-    print(input_data.shape)
-    print(module.get_input_details())
+    print(f"Frame.shape = {frame.shape}")
+    print(f"Input_data.shape = {input_data.shape}")
 
     # Invoking model
-    module.set_tensor(input_dtl[0]["index"], input_data)
+    module.set_tensor(input_details[0]["index"], input_data)
     module.invoke()
 
-    # Getting output tensor
-    module.set_tensor(input_dtl[0]['index'], input_data)
-    module.invoke()
-    pred_boxes = module.get_tensor(output_dtl[0]['index'])
-    #pred_scores = module.get_tensor(output_dtl[1]['index'])
-    # pred_classes = module.get_tensor(output_dtl[2]['index'])
-    # num_boxes = int(module.get_tensor(output_dtl[3]['index'])[0])
-    print(f"Output_pred_box: {pred_boxes}")
-    print(f"Output_dtl: {output_dtl}")
+    # Getting output
+    boxes = module.get_tensor(output_details[0]["index"])[0]  # Bounding box coordinates
+    class_indices = module.get_tensor(output_details[1]["index"])[0].astype(int)
+
+    # Map class indices to class names
+    classes = [labels[i] for i in class_indices]
+
+    print(f"Output_pred_box: {boxes}")
+    print(f"Output_dtl: {output_details}")
+
 cap.release()
 cv2.destroyAllWindows()
-
